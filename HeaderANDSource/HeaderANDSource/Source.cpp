11552d59784d813c0d2742c526a94640e6590969
woooyoung <axdsw121@gmail.com>
2021-09-09 10:19:38 +0900

    HeaderANDSource

---
+++ HeaderANDSource/HeaderANDSource/Source.cpp
@@ -0,0 +1,29 @@
+﻿#include "헤더.h"
+
+int main() {
+
+	int v1 = 100;
+	int v2 = 200;
+
+	int rs = add(v1, v2);
+
+	printf("%d\n", rs);
+
+	Person p1;
+	p1.age = 22;
+
+	printf("p1.age : %d\n", p1.age);
+
+	//printf("%f\n",pow(2.0,10));
+
+	//printf("%f\n", sin(1));
+
+	//srand(time(0));
+	//printf("%d\n", rand() % 10);
+
+	return 0;
+}
+
+int add(int a, int b) {
+	return a + b;
+}
---
+++ HeaderANDSource/HeaderANDSource/헤더.h
@@ -0,0 +1,13 @@
+﻿#include <cstdio>
+#include <cmath>  // sin(), cos(), tan(), exp(), pow(), log()
+#include <cstdlib> // malloc, free, rand
+#include <ctime> // 의사난수 시드 설정
+
+
+struct Person
+{
+	int age;
+	char name[100];
+};
+
+int add(int a, int b);
