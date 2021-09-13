1fda5a253220732ffb6963c364cc750d328ca661
woooyoung <axdsw121@gmail.com>
2021-09-13 10:34:32 +0900

    CLASS_OOP

---
+++ CLASS_OOP/CLASS_OOP/소스.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdio>
+
+// 회사의 erp 시스템
+// 나이, 봉급, 이름, 혈액형, 이메일주소, 주민번호, 3대(벤치,스퀏,데드), 결혼여부, 자녀유무, 유튜브 닉네임
+// 나이, 봉급, 이름, 이메일주소, 주민번호, 결혼여부, 자녀유무
+struct Person {
+	int age;
+	float salary;
+	char name[100];
+
+	void goToWork() {};
+	void WorkOff() {};
+	void vacation() {};
+	//void gameInHome() {};
+}; // 추상화(abstraction)
+
+
+int main() {
+
+
+	return 0;
+}
---
+++ CLASS_OOP/CLASS_OOP/소스1.cpp
@@ -0,0 +1,42 @@
+﻿// 상속(inheritance)
+
+struct Animal {
+	int age = 1;
+	int legs = 0;
+	int arms = 0;
+};
+
+struct Person : public Animal {
+	Person() {
+		int legs = 2;
+		int arms = 2;
+	}
+};
+
+struct Tiger : public Animal {
+	Tiger() {
+		int age = 11;
+		int legs = 4;
+	}
+};
+
+
+#include <cstdio>
+
+int main() {
+
+	Person p1;
+	p1.age = 0;
+	p1.arms = 0;
+	p1.legs = 0;
+
+	Person* person;
+
+	Animal* animal;
+
+	//animal = person;
+
+	person = (Person*)animal;  // casting -> 형변환
+
+	return 0;
+}
