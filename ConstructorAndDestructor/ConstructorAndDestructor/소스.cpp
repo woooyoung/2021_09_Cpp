9a4e5cb280e3327fe723d27ed4e6c94fcb002be1
woooyoung <axdsw121@gmail.com>
2021-09-14 10:21:46 +0900

    ConstructorAndDestructor_03

--- ConstructorAndDestructor/ConstructorAndDestructor/소스.cpp
+++ ConstructorAndDestructor/ConstructorAndDestructor/소스.cpp
@@ -10,13 +10,19 @@
 class Student {
 public:
 	int student_no;
-	char name[100];
+	char* name;   //name[100]
 	int gender; // 0-남 1-여
 
-	Student(int StdNo, const char* n, int g) {
-		this->student_no = student_no;
+	Student(int StdNo, const char* n, int g) {  // 생성자: 생성되자마자 파라미터를 이용해서 값을 초기화해 넣어준다.
+		this->student_no = StdNo;
+		name = new char[strlen(n) + 1];
 		strcpy(name, n);
 		gender = g;
+	}
+
+	~Student() { // 소멸자 : 클래스 인스턴스가 파괴될 때 -> 메모리에서 해제될 때
+		printf("소멸자 호출\n");
+		delete[] name;
 	}
 
 	void printStudentInfo() {
@@ -37,6 +43,8 @@ int main() {
 	Student* ps = new Student(5678,"임꺽정",0);
 
 	ps->printStudentInfo();
+
+	delete ps;
 
 	//Student s1;
 	//s1.student_no = 1234;
