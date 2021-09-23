b846e7d378b64dbd7967d29652b8a57503da20b9
woooyoung <axdsw121@gmail.com>
2021-09-23 10:45:09 +0900

    오버로딩 & 오버라이딩

--- OverrideANDOverload/OverrideANDOverload/소스2.cpp
+++ OverrideANDOverload/OverrideANDOverload/소스2.cpp
@@ -1,6 +1,6 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 //연산자 오버로딩   
-//+ - * / -> 기본 자료형에만 유효한 연산자
+//+ - * / -> 기본 자료형에만 유효한 연산자, 클래스나 다른 자료형은 유효하지 않다.
 #include <cstdio>
 #include <cstring>
 
@@ -19,14 +19,26 @@ public:
 		this->Y = y;
 	}
 
+	void printInfo(float x, float y) {
+		printf("%.2f, %.2f\n", x, y);
+	}
+
 };
+
+//벡터 덧셈
+//Vector operator+(Vector v1, Vector v2) { //연산자 오버로딩 : 정적으로 할당(스택얼로케이티드) 했을때만 동작한다.
+Vector operator+(Vector& v1, Vector& v2) { // 값복사가 일어나지 않는다. 주소복사 -> 퍼포먼스가 좋다.
+	return Vector(v1.X + v2.X, v1.Y + v2.Y);
+}
 
 int main() {
 
 	Vector v1 = Vector(100,200);
 	Vector v2 = Vector(1,2);
 
-	Vector v3 = v1 + v2;
+	Vector v3 = v1 + v2;  
+
+	v3.printInfo(v3.X,v3.Y);
 
 	return 0;
 }
---
+++ OverrideANDOverload/OverrideANDOverload/소스3.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+//벡터와 스칼라 곱
+#include <cstdio>
+#include <cstring>
+//3차원 벡터
+class Vector3 {
+public:
+	float X;		
+	float Y;
+	float Z;
+
+	Vector3() {
+		X = 0.0f;
+		Y = 0.0f;
+		Z = 0.0f;
+	}
+
+	Vector3(float x, float y, float z) {
+		this->X = x;
+		this->Y = y;
+		this->Z = z; 
+	}
+
+	void printInfo(float x, float y, float z) {
+		printf("%.2f, %.2f, %.2f\n", x, y, z);
+	}
+
+};
+
+Vector3 operator*(Vector3& v1, float s) {
+	return Vector3(v1.X * s, v1.Y * s, v1.Z * s); 
+}
+
+int main() {
+
+	Vector3 v = Vector3(10,20,30);
+
+	Vector3 rs = v * 10;
+
+	rs.printInfo(rs.X,rs.Y,rs.Z);
+
+	return 0;
+}
