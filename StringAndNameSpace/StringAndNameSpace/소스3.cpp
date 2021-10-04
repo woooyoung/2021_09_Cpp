fc63dadc817e151385258728b4b9a7bb0727f51e
woooyoung <axdsw121@gmail.com>
2021-10-04 10:31:08 +0900

    StringAndNameSpace

---
+++ StringAndNameSpace/StringAndNameSpace/소스3.cpp
@@ -0,0 +1,50 @@
+﻿//namespace
+#include <iostream>
+
+namespace dong {
+	void exam1();
+
+	class Person {
+	public:
+		Person();
+
+		void printInfo(int i, const char* str);
+	};
+}
+
+namespace sun {
+	void exam1();
+
+	class Person {
+	public:
+		Person();
+	};
+}
+
+int main() {
+
+	dong::exam1();
+	sun::exam1();
+
+	return 0;
+}
+
+void dong::exam1() {
+	printf("홍길동이 만든 함수 exam1이다.\n");
+}
+
+void sun::exam1() {
+	printf("홍길순이 만든 함수 exam1이다.\n");
+}
+
+dong::Person::Person() {
+
+}
+
+sun::Person::Person() {
+
+}
+
+void dong::Person::printInfo(int i, const char* str) {
+
+}
---
+++ StringAndNameSpace/StringAndNameSpace/소스4.cpp
@@ -0,0 +1,15 @@
+﻿#include <cstdio>
+#include <vector>
+#include <string>
+
+using namespace std; // 이 코드가 나온 시점부터 전부다 std라는 네임스페이스를 생략하겠다.
+
+int main() {
+	string myStr = "Hello";
+
+	vector<int> myvec;
+
+	printf("%s\n", myStr.c_str());
+
+	return 0;
+}
