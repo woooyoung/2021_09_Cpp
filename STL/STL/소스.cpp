44fa1441624c51a6d6482dc116e43716b4f7d4f0
woooyoung <axdsw121@gmail.com>
2021-10-06 10:52:05 +0900

    STL

---
+++ STL/STL/소스.cpp
@@ -0,0 +1,48 @@
+﻿#include <cstdio>
+#include <string>
+
+template <typename T>
+class MyClass {		// 이 클래스 내부에는 타입이 지정되지 않은 무언가가 있다 -> 타입의 지정을 뒤로 미루겠다
+public:
+	T value;
+};
+
+template <typename T>
+void swap(T* a, T* b) {
+	T temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
+int main() {
+
+	MyClass<int>* m = new MyClass<int>();
+	m->value = 100;
+
+	printf("%d\n", m->value);
+
+	MyClass<std::string>* ms = new MyClass<std::string>();
+	ms->value = "Hello World!";
+
+	printf("%s\n", ms->value.c_str());
+
+	int value1 = 100;
+	int value2 = 200;
+
+	swap<int>(&value1, &value2);
+
+	printf("value1 : %d\n", value1);
+	printf("value2 : %d\n", value2);
+
+	std::string svalue1 = "Hi";
+	std::string svalue2 = "Bye";
+
+	swap<std::string>(&svalue1, &svalue2);
+
+	printf("svalue1 : %s\n", svalue1.c_str());
+	printf("svalue2 : %s\n", svalue2.c_str());
+
+	return 0;
+}
---
+++ STL/STL/소스1.cpp
@@ -0,0 +1,58 @@
+﻿//가변배열
+#include <cstdio>
+#include <string>
+
+template <typename T>
+class MyArray {
+public:
+	T* arrayItem;	//	배열 포인터
+	int count = 0;	// 배열 요소 몇개?
+	int capacity = 8;	//	배열이 최대 한도로 가질 수 있는 크기
+
+	MyArray() {
+		arrayItem = new T[capacity];
+	}
+
+	virtual ~MyArray() {
+		delete[] arrayItem;
+	}
+
+	void putValue(T value) {
+		if (capacity > count) {
+			arrayItem[count] = value;
+			count++;
+		}
+		else {
+			printf("배열의 capacity 두배로 늘어남\n");
+			T* newArray = new T[capacity * 2];
+			capacity = capacity * 2;
+			for (int i = 0; i < count; i++) {
+				newArray[i] = arrayItem[i];
+			}
+			arrayItem = newArray;
+			arrayItem[count] = value;
+			count++;
+		}
+	}
+};
+
+int main() {
+
+	MyArray<int> m = MyArray<int>();
+
+	m.putValue(100);
+	m.putValue(200);
+	m.putValue(300);
+	m.putValue(400);
+	m.putValue(500);
+	m.putValue(600);
+	m.putValue(700);
+	m.putValue(800);
+	m.putValue(900);
+
+	for (int i = 0; i < m.count; i++) {
+		printf("%d ", m.arrayItem[i]);
+	}
+
+	return 0;
+}
