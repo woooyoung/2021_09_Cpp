92bd72df207c52c9200d2ca41206aa0335fa1997
woooyoung <axdsw121@gmail.com>
2021-10-08 11:03:38 +0900

    TETRIS02

--- TETRIS/TETRIS/Source.cpp
+++ TETRIS/TETRIS/Source.cpp
@@ -7,6 +7,7 @@ int main() {
 	float prev = (float)clock() / CLOCKS_PER_SEC;
 	//clock() 이 프로그램 시작된 순간부터 계속 시간을 구한다.
 	Display* display = new Display();
+	GameEngine* gameEngine = new GameEngine();
 	showConsoleCursor(false); // 언더바 지우는 함수
 
 	while (true) {
@@ -19,13 +20,26 @@ int main() {
 		//printf("%.3f\n", curr);
 
 		bool left = keyState('a');
+		bool right = keyState('d');
+		bool down = keyState('s');
 		if (left) {
 			//왼쪽으로 블록을 이동시킨다.
+			gameEngine->next(dt, 'a');
+		}
+		else if (right) {
+			gameEngine->next(dt, 'd');
+		}
+		else if (down) {
+			gameEngine->next(dt, 's');
 		}
 		else {
 			//그냥 떨어진다.
+			gameEngine->next(dt, 0);
 		}
+
+		/*gameEngine->next(dt, 0);*/
 		//화면에 출력
+		gameEngine->makeDisplayData();
 		display->draw();
 		//게임상태를 판별
 	}
